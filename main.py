b4b36e26826d4ad0fc231270c69f030912826ef1
Dávid Cirok <cirokdavid@gmail.com>
2018-11-17 23:41:48 +0100

    segment lines & paragraphs

--- main.py
+++ main.py
@@ -4,29 +4,63 @@ from PyQt5.QtWidgets import (
     QVBoxLayout,
     QHBoxLayout,
     QLabel,
+    QLineEdit,
+    QFormLayout,
     QPushButton,
     QFileDialog
 )
+
+from PyQt5.QtGui import QIntValidator
 
 from process_img import process
 
 # basic GUI setup
 app = QApplication(['Advanced Image Processing Project'])
 window = QWidget()
-window.setFixedSize(500, 50)
-layout = QHBoxLayout()
+window.setFixedSize(500, 100)
+layout = QVBoxLayout()
+flo = QFormLayout()
+bottom = QHBoxLayout()
+layout.addLayout(flo)
+layout.addLayout(bottom)
 
-layout.addWidget(QLabel('Select an image to process!'))
+# setup parameter inputs
+lineCloseKernelWidth = 40
+paragraphCloseKernelHeight = 10
+
+def lckwChanged(val):
+    if val != '':
+        global lineCloseKernelWidth
+        lineCloseKernelWidth = int(val)
+
+def pckwChanged(val):
+    if val != '':
+        global paragraphCloseKernelHeight
+        paragraphCloseKernelHeight = int(val)
+
+e1 = QLineEdit()
+e1.setValidator(QIntValidator(1, 100))
+e1.setText('40')
+e1.textChanged.connect(lckwChanged)
+e2 = QLineEdit()
+e2.setValidator(QIntValidator(1, 50))
+e2.setText('10')
+e2.textChanged.connect(pckwChanged)
+
+flo.addRow('Kernel height for joining paragraphs', e2)
+flo.addRow('Kernel width for joining lines', e1)
 
 # setup open image button
 def selectImage():
     path = QFileDialog.getOpenFileName()[0]
     if path != '':
-        process(path)
+        process(path, lineCloseKernelWidth, paragraphCloseKernelHeight)
+
+bottom.addWidget(QLabel('Select an image to process!'))
 
 openFileButton = QPushButton('Open an image')
 openFileButton.clicked.connect(selectImage)
-layout.addWidget(openFileButton)
+bottom.addWidget(openFileButton)
 
 # run the GUI
 window.setLayout(layout)
--- process_img.py
+++ process_img.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 
 # main image processing function
-def process(path):
+def process(path, lineCloseKernelWidth = 40, paragraphCloseKernelHeight = 10):
     # (1) read
     img = cv2.imread(path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -23,24 +23,30 @@ def process(path):
     # (4) Find rotated matrix, do rotation
     M = cv2.getRotationMatrix2D((cx, cy), ang, 1.0)
     rotated = cv2.warpAffine(threshed, M, (img.shape[1], img.shape[0]))
+    rotatedOriginal = cv2.warpAffine(img, M, (img.shape[1], img.shape[0]))
 
-    # (5) find and draw the upper and lower boundary of each lines
-    hist = cv2.reduce(rotated, 1, cv2.REDUCE_AVG).reshape(-1)
+    # (5) perform a morph. close to segment the lines
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (lineCloseKernelWidth,1))
+    closed = cv2.morphologyEx(rotated, cv2.MORPH_CLOSE, kernel)
+    ret, contours, hierarchy = cv2.findContours(closed,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        x,y,w,h = cv2.boundingRect(cnt)
+        cv2.rectangle(ret,(x,y),(x+w,y+h),(255,255,255),-1)
+        cv2.rectangle(rotatedOriginal,(x,y),(x+w,y+h),(0,255,0),2)
 
-    th = 2
-    H, W = img.shape[:2]
-    uppers = [y for y in range(H - 1) if hist[y] <= th and hist[y + 1] > th]
-    lowers = [y for y in range(H - 1) if hist[y] > th and hist[y + 1] <= th]
+    # (6) perform a morph. close to segment the paragraphs
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1,paragraphCloseKernelHeight))
+    closed = cv2.morphologyEx(ret, cv2.MORPH_CLOSE, kernel)
+    ret, contours, hierarchy = cv2.findContours(closed,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        x,y,w,h = cv2.boundingRect(cnt)
+        cv2.rectangle(rotatedOriginal,(x,y),(x+w,y+h),(0,0,255),3)
 
-    rotated = cv2.cvtColor(rotated, cv2.COLOR_GRAY2BGR)
-    for y in uppers:
-        cv2.line(rotated, (0, y), (W, y), (255, 0, 0), 1)
+    # (7) rotate the annotated image back to the original angle
+    M = cv2.getRotationMatrix2D((cx, cy), -ang, 1.0)
+    result = cv2.warpAffine(rotatedOriginal, M, (img.shape[1], img.shape[0]))
 
-    for y in lowers:
-        cv2.line(rotated, (0, y), (W, y), (0, 255, 0), 1)
-
-    # cv2.imwrite("result.png", rotated)
-    cv2.imshow('result', rotated)
+    cv2.imshow('result', result)
 
 
 # when run as a script, the image path comes from a command line argument
